51e78f5e349d3aea647b0d11b8dfd2d87f1028b7
mrsomebody02 <iliecristian12@gmail.com>
2023-03-05 03:42:57 +0200

    1st day

--- api/products.php
+++ api/products.php
@@ -2,41 +2,41 @@
 header("Access-Control-Allow-Origin: *");
 
 class Products {
-  private $servername;
-  private $username;
-  private $password;
-  private $dbname;
-  private $conn;
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
+    private $conn;
 
-  public function __construct() {
-    $this->servername = "localhost";
-    $this->username = "root";
-    $this->password = "2468";
-    $this->dbname = "scandiweb";
-    $this->conn = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname);
-  }
-
-  public function get_products() {
-    if (!$this->conn) {
-      die("Connection failed: " . mysqli_connect_error());
+    public function __construct() {
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "2468";
+        $this->dbname = "scandiweb";
+        $this->conn = mysqli_connect($this->servername, $this->username, $this->password, $this->dbname);
     }
 
-    $sql = "SELECT p.*, a.attribute_type, a.attribute_value FROM products p 
-    LEFT JOIN product_attributes a ON p.id = a.product_id";
-    $result = mysqli_query($this->conn, $sql);
+    public function get_products() {
+        if (!$this->conn) {
+        die("Connection failed: " . mysqli_connect_error());
+        }
 
-    if (mysqli_num_rows($result) > 0) {
-      $rows = array();
-      while($row = mysqli_fetch_assoc($result)) {
-        $rows[] = $row;
-      }
-      echo json_encode($rows);
-    } else {
-      echo "No products found.";
+        $sql = "SELECT p.*, a.attribute_type, a.attribute_value FROM products p 
+        LEFT JOIN product_attributes a ON p.id = a.product_id";
+        $result = mysqli_query($this->conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+        $rows = array();
+        while($row = mysqli_fetch_assoc($result)) {
+            $rows[] = $row;
+        }
+        echo json_encode($rows);
+        } else {
+        echo "No products found.";
+        }
+
+        mysqli_close($this->conn);
     }
-
-    mysqli_close($this->conn);
-  }
 }
 
 // Instantiate the class and call the get_products method
--- scandiweb/src/components/test/test.jsx
+++ scandiweb/src/components/test/test.jsx
@@ -1,35 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './test.css'
 
 
 function Test() {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost/products.php')
-      .then(response => {
-        setProducts(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, []);
-  console.log(products);
-  return (
-    <div>
-      <h1>Product List</h1>
-      <ul>
-        {products.map(product => (
-          <li key={product.id}>
-            <h2>{product.name}</h2>
-            <p>SKU: {product.sku}</p>
-            <p>Price: ${product.price}</p>
-            <p>{product.attribute_type}: {product.attribute_value} {product.attribute_unit}</p>
-          </li>
-        ))}
-      </ul>
+    useEffect(() => {
+        axios.get('http://localhost/products.php')
+        .then(response => {
+            setProducts(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }, []);
+
+    console.log(products);
+    return (
+    <div className='container'>
+        <ul>
+            {products.map(product => (
+            <div className='containerPost'>
+            <li key={product.id}>
+                <p className='sku'>SKU: {product.sku}</p>
+                <p className='name'>{product.name}</p>
+                <p className='price'>Price: ${product.price}</p>
+                <p className='attributes'>{product.attribute_type}: {product.attribute_value} {product.attribute_unit}</p>
+            </li>
+            </div>
+            ))}
+        </ul>
     </div>
-  );
+            
+
+
+    )
 }
 
 export default Test;
+
+
+
+
