e59ae8937c17b8309b60d4807ebadab612fb83e7
mrsomebody02 <iliecristian12@gmail.com>
2023-03-06 02:12:16 +0200

    crash 3
    god this is so anno...

--- scandiweb/src/page/addProduct/addProduct.jsx
+++ scandiweb/src/page/addProduct/addProduct.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './addproduct.css';
 import {  Link } from 'react-router-dom';
 import axios from 'axios';
+import Select from 'react-select'
 
 const addProduct = () => {
+
+    const options = [
+        { value: 'DVD', label: 'DVD' },
+        { value: 'Book', label: 'Book' },
+        { value: 'Furniture', label: 'Furniture' }
+    ]
+    
+    const [selectedProductType, setSelectedProductType] = useState('DVD');
+
+    const handleSelectChange = (event) => {
+        setSelectedProductType(event.target.value);
+    };
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className='container'>
         <div className='headerProductList'>
@@ -13,7 +37,7 @@ const addProduct = () => {
             
             <div className='buttonsContainer'>
                 <div className='buttons'>
-                    <Link to='/'  id='add-product-btn'>
+                    <Link form='formProduct' type='submit' value='update' to='/'  id='add-product-btn'>
                         <p className='buttonsName'>Save</p>
                     </Link>
                 </div>
@@ -25,8 +49,28 @@ const addProduct = () => {
             </div>
         </div>
         {/* ------------------------------------------------------------------------------------- */}
-        <div className='containerProducts'>
-        
+        <div className='containerAdd'>
+            <form id='formProduct' className='formContainer'>
+                <label>
+                    <div  className='inputName'>
+                        <p>SKU</p>
+                        <input id='sku' type="text" sku="sku" />
+                    </div>
+                    <div  className='inputName'>
+                        <p>Name</p>
+                        <input id='name' type="text" name="name" />
+                    </div>
+                    <div  className='inputName'>
+                        <p>Price($)</p>
+                        <input id='price' type="text" price="name" />
+                    </div>
+                    
+                </label>
+                <div className='dropdownSwitch'>
+                <Select id='productType' options={options} />
+                </div>
+                    {/* <input className='sumitBtn' type="submit" value="Submit" /> */}
+            </form>
         </div>
     </div>
   )
